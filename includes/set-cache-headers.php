d5a9d02b86e56dfddb9ac9da449c2d683b3ac61e
Danny van Kooten <hi@dvk.co>
2026-04-18 12:49:28 +0200

    cache sitemap for 1 day

--- includes/set-cache-headers.php
+++ includes/set-cache-headers.php
@@ -19,7 +19,10 @@ add_filter('wp_headers', function ($headers) {
     } elseif (is_404()) {
         $headers['Cache-Control'] = 'public, max-age=3600';
 
-    // cache all other pages for 30 days
+    // cache feeds and XML files (ie sitemap) for 1 day
+    } elseif (is_feed() || str_ends_with($_SERVER['REQUEST_URI'] ?? '', '.xml')) {
+        $headers['Cache-Control'] = 'public, max-age=81600';
+    // cache all other pages for 1 year
     } else {
         $headers['Cache-Control'] = 'public, max-age=2592000';
     }
