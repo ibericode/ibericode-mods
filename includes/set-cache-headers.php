f02570de4be0296fbfeed6d421530e26483fc33b
Danny van Kooten <hi@dvk.co>
2026-05-12 11:05:37 +0200

    shorten browser cache length to 1 hour, shared is already close enough

--- includes/set-cache-headers.php
+++ includes/set-cache-headers.php
@@ -18,16 +18,17 @@ add_filter('wp_headers', function ($headers) {
     if (is_user_logged_in()) {
         $headers['Cache-Control'] = 'must-revalidate, max-age=0, private';
 
-    // cache 404 pages for 1 hour (shared), 5 minutes (browser)
+    // cache 404 pages for 1 hour (shared) or 5 minutes (browser)
     } elseif (is_404()) {
         $headers['Cache-Control'] = 'public, s-max-age=3600, max-age=300';
 
-    // cache feeds and XML files (ie sitemap) for 1 day (both shared and browser)
+    // cache feeds and XML files (ie sitemap) for 1 day (shared) or 1 hour (browser)
     } elseif (is_feed() || str_ends_with($_SERVER['REQUEST_URI'] ?? '', '.xml')) {
-        $headers['Cache-Control'] = 'public, max-age=86400';
-    // cache all other pages for 30 days (shared) or 1 day (browser cache)
+        $headers['Cache-Control'] = 'public, s-max-age=86400, max-age=3600';
+
+    // cache all other pages for 30 days (shared) or 1 hour (browser)
     } else {
-        $headers['Cache-Control'] = 'public, s-max-age=2592000, max-age=86400';
+        $headers['Cache-Control'] = 'public, s-max-age=2592000, max-age=3600';
     }
 
     return $headers;
