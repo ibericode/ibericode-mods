dd46b11f036cae1807a1d8c11e07820167d54087
Danny van Kooten <hi@dvk.co>
2026-05-12 09:55:36 +0200

    specify Update URI

--- ibericode-mods.php
+++ ibericode-mods.php
@@ -5,15 +5,20 @@ Plugin Name: ibericode mods
 Description: a collection of commonly needed WordPresss modifications or enhancements
 Author: Danny van Kooten
 Version: 1.0
-Author URI: https://dannyvankooten.com/
+Author URI: https://www.dannyvankooten.com/
 Private: True
 Requires at least: 6.5
 Requires PHP: 8.3 
 License: GPL v2 or later
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
+Update URI: https://www.ibericode.com/
 */
 
 if (PHP_VERSION_ID < 80300) {
+    return;
+}
+
+if (! defined('ABSPATH')) {
     return;
 }
 
