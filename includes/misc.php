870f50b08a913b67d71956224af6adaf9b6acd6e
Danny van Kooten <hi@dvk.co>
2026-06-12 10:44:06 +0200

    ignore phpstan error

--- includes/misc.php
+++ includes/misc.php
@@ -7,16 +7,15 @@ defined('ABSPATH') or exit;
  */
 add_action('load-plugins.php', static function () {
 
-    if (! defined('DISALLOW_FILE_MODS') || false === DISALLOW_FILE_MODS) {
+    if (wp_is_file_mod_allowed('install_plugins')) {
         return;
     }
 
     $plugins = get_site_transient('update_plugins');
-
     if (isset($plugins->response) && is_array($plugins->response)) {
         $plugins = array_keys($plugins->response);
         foreach ($plugins as $plugin_file) {
-            add_action("after_plugin_row_$plugin_file", 'wp_plugin_update_row', 10, 2);
+            add_action("after_plugin_row_$plugin_file", 'wp_plugin_update_row', 10, 2); /* @phpstan-ignore return.void */
         }
     }
 }, 30);
